30b9c0b1d584a4784f2870a3cbe6910289fa441d
tima <islamtemirlan@gmail.com>
2025-03-07 02:40:42 +0500

    lab6

---
+++ lab6/dif-and-files.py/1-exercise.py
@@ -0,0 +1,5 @@
+import os
+
+path= r"C:\Users\Admin\Git\Programming-Principles-2\docs"
+all=list(os.listdir(path))
+print(all)
---
+++ lab6/dif-and-files.py/3-exercise.py
@@ -0,0 +1,9 @@
+import os
+path= r"C:\Users\Admin\Git\Programming-Principles-2\docs"
+def checker(path):
+    if os.path.exists(path):
+        print("Name of file: ", os.path.basename(path))
+        print("name of directory: ", os.path.dirname(path))
+        return "success"
+    
+print(checker(path))
